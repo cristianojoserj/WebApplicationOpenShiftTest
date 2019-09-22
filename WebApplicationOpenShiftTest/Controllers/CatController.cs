819ef1b5d28564d854f506b85dcdc03de22595d4
Cristiano Pereira <cristiano.pereira@osbr.com.br>
2019-09-22 20:09:10 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplicationOpenShiftTest/Controllers/CatController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationOpenShiftTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatController : ControllerBase
+    {
+        [HttpGet("feline/bill")]
+        public string Meow()
+        {
+            return "Meow!";
+        }
+    }
+}
